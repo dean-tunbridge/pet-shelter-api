27357230574d11f6782005d69621ec7ecde721b8
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-02 12:19:37 +1200

    tested script and response data

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const pet = { name: 'Pixel', species: 'cat', adopted: true, age: 9 };
+const pets = [
+    { name: 'Pixel', species: 'cat', adopted: true, age: 9 },
+    { name: 'Miso', species: 'cat', adopted: true, age: 6 },
+    { name: 'Leonard', species: 'dog', adopted: false, age: 7 },
+];
 const app = (0, express_1.default)();
 const PORT = 8000;
 //GET
 app.get('/', (req, res) => {
-    res.json(pet);
+    res.json(pets);
 });
 app.listen(PORT, () => {
     console.log(`Listening on port:${PORT}`);
