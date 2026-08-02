f9e12be31107063e8c9aa29f30a3f27deaa8469d
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-03 09:17:08 +1200

    add types to response for get request and 404

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import type { Express, Request, Response } from 'express'
 import cors from 'cors'
 
-import { pets } from './data/pets'
+import { pets, Pets } from './data/pets'
 
 const app: Express = express()
 const PORT = 8000
@@ -10,11 +10,11 @@ const PORT = 8000
 app.use(cors())
 
 //GET
-app.get('/', (req: Request, res: Response): void => {
+app.get('/', (req: Request, res: Response<Pets[]>): void => {
   res.json(pets)
 })
 
-app.use((req: Request, res: Response): void => {
+app.use((req: Request, res: Response<{ message: string }>): void => {
   res.status(404).json({ message: 'No route found' })
 })
 
