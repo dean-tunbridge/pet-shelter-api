c6c26da90e7d8d01bafaf4f1e666b267d33ae417
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-02 15:52:59 +1200

    fixed issue with cors install

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const pets_1 = require("./data/pets");
 const app = (0, express_1.default)();
 const PORT = 8000;
+app.use((0, cors_1.default)());
 //GET
 app.get('/', (req, res) => {
     res.json(pets_1.pets);
