9899b5a072b632ac35cf826a26cb9a56fd7597d2
Dean Tunbridge <deantunbridgedev@gmail.com>
2026-08-02 12:09:36 +1200

    create test pet and create pet type

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import express from 'express'
 
 import type { Express } from 'express'
 
+type Pets = {
+  name: string
+  species: string
+  adopted: boolean
+  age: number
+}
+
+const pet: Pets = { name: 'Pixel', species: 'cat', adopted: true, age: 9 }
+
 const app: Express = express()
 const PORT = 8000
 
 //GET
 app.get('/', (req, res) => {
-  res.json({})
+  res.json({ pet })
 })
 
 app.listen(PORT, (): void => {
